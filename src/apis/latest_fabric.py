3867176fb8267b0d03fd6b072bc6f2e57ba19288
shouko.-. <144153287+skotish06@users.noreply.github.com>
2025-10-05 16:37:41 +0700

    latest version for fabric

---
+++ src/apis/latest_fabric.py
@@ -0,0 +1,14 @@
+import requests
+
+def latest_fabric_ver(mc_version: str) -> str:
+    url = f"https://meta.fabricmc.net/v2/versions/loader/{mc_version}/"
+    r = requests.get(url)
+    r.raise_for_status()
+    data = r.json()
+
+    if not data:
+        raise ValueError(f"Not found {mc_version}")
+
+    latest_loader = data[0]["loader"]["version"]
+    return latest_loader
+
