4670db898d6d64ba8cebed475ffa0c66a08f8fe4
shouko.-. <144153287+skotish06@users.noreply.github.com>
2025-10-06 09:03:02 +0700

    fabric and forge installation

--- src/cli.py
+++ src/cli.py
@@ -4,18 +4,47 @@ import download
 import installer
 from templates import eula
 import start
+import json
+import os
+
+def set_tag(folder_path, tag_name):
+    tag_file = os.path.join(folder_path, ".tag")
+    with open(tag_file, "w") as f:
+        json.dump({"tag": tag_name}, f)
+
+def get_tag(folder_path):
+    tag_file = os.path.join(folder_path, ".tag")
+    try:
+        with open(tag_file, "r") as f:
+            data = json.load(f)
+        return data.get("tag")
+    except (json.JSONDecodeError, OSError, KeyError):
+        return None
 
 def main():
     args = handler.info()
     if args.command == "create":
         folder.init(args)
+        if args.modloader == "forge":
+            set_tag(args.folder, "forge")
+        elif args.modloader == "fabric":
+            set_tag(args.folder, "fabric")
+
+        # print(args.folder)
         file_name = download.downloader(args.version, args.modloader, args.mldver, args.folder)
+        print(file_name)
         if (args.modloader == "forge"):
             installer.installServer(args.folder, file_name)
         eula.write_eula(args.folder)
 
     elif args.command == "start":
-        start.start_forge_server(args.folder)
+        # print(get_tag(args.folder))
+        if (get_tag(args.folder) == "forge"):
+            start.start_forge_server(args.folder)
+        elif (get_tag(args.folder) == "fabric"):
+            start.start_fabric_server(args.folder)
+        else:
+            print("Unknown command")
 
 
 if __name__ == "__main__":
--- src/start.py
+++ src/start.py
@@ -1,4 +1,13 @@
 import subprocess
+import os
 
 def start_forge_server(folder):
     subprocess.run(["./run.sh"], cwd=folder)
+
+def start_fabric_server(folder):
+    jar_files = [f for f in os.listdir(folder) if f.endswith(".jar")]
+    if not jar_files:
+        print("Cant find jar file")
+        return
+    jar_file = jar_files[0]
+    subprocess.run(["java", "-Xms1G", "-Xmx4G", "-jar", jar_file, "nogui"],cwd=folder)
